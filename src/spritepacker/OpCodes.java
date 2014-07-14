e53781f5e00c0ad863de15a4e29e59d4d101d976
ShacoAWU <walibi-psm@hotmail.com>
2014-07-15 00:52:52 +0200

    Update OpCodes.java

--- src/spritepacker/OpCodes.java
+++ src/spritepacker/OpCodes.java
@@ -1,14 +1,26 @@
 package spritepacker;
 
 public enum OpCodes {
+    /**
+     * The OpCodes that will be used to read and write the cache
+     */
     FILE_NAME(1), FILE_LENGTH(2) ,FILE_DATA(3);
 
+    /**
+     * Holds the specific OpCode.
+     */
     private int opCode;
 
+    /**
+     * Creates a new OpCode.
+     */
     private OpCodes(int opCode) {
         this.opCode = opCode;
     }
 
+    /**
+     * Returns the value of the OpCode.
+     */
     public int getOpCode() {
         return opCode;
     }
